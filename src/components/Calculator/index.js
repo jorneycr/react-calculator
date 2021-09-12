f7005f92d53f7f793e876e4e8b170398678b8a34
jorneylm <jorney95@live.com>
2021-09-11 20:07:05 -0600

    calculator done

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -1,33 +1,103 @@
-import React from 'react';
-import { Container, Screen,Previous, Current, Button } from './Styled';
+import React, { useState } from 'react';
+import { Container, Screen, Previous, Current, Button } from './Styled';
 
 const Calculator = () => {
-    return ( 
+
+    const [previous, setPrevious] = useState('');
+    const [current, setCurrent] = useState('');
+    const [operation, setOperation] = useState('');
+
+    const appendValue = (el) => {
+        const value = el.target.getAttribute('data')
+        if (value === '.' && current.includes('.')) return;
+        setCurrent(current + value);
+    }
+
+    const handleDelete = () => {
+        setCurrent(String(current).slice(0, -1));
+    }
+
+    const handleAllClear = () => {
+        setCurrent('');
+        setPrevious('');
+        setOperation('');
+    }
+
+    const chooseOperation = (el) => {
+        if (current === '') return
+
+        if (previous !== '') {
+            let value = compute();
+            setPrevious(value);
+        } else {
+            setPrevious(current);
+        }
+
+        setCurrent('');
+        setOperation(el.target.getAttribute('data'));
+    }
+
+    const compute = () => {
+        let result;
+        let previousNumber = parseFloat(previous);
+        let currentNumber = parseFloat(current);
+
+        if (isNaN(previousNumber) || isNaN(currentNumber)) return
+
+        switch (operation) {
+            case '/':
+                result = previousNumber / currentNumber;
+                break;
+            case '*':
+                result = previousNumber * currentNumber;
+                break;
+            case '-':
+                result = previousNumber - currentNumber;
+                break;
+            case '+':
+                result = previousNumber + currentNumber;
+                break;
+            default:
+                return
+        }
+        return result;
+    }
+
+    const equals = () => {
+        let value = compute();
+        if (value === undefined || value == null) return;
+
+        setCurrent(value);
+        setPrevious('');
+        setOperation('');
+    }
+
+    return (
         <Container>
             <Screen>
-                <Previous>1 +</Previous>
-                <Current>2</Current>
+                <Previous>{previous} {operation}</Previous>
+                <Current>{current}</Current>
             </Screen>
-            <Button gridSpan={2} color={'skyblue'}>AC</Button>
-            <Button color={'skyblue'}>DEL</Button>
-            <Button color={'gray'}>/</Button>
-            <Button>7</Button>
-            <Button>8</Button>
-            <Button>9</Button> 
-            <Button color={'gray'}>*</Button>           
-            <Button>4</Button>
-            <Button>5</Button>
-            <Button>6</Button>
-            <Button color={'gray'}>+</Button>
-            <Button>1</Button>
-            <Button>2</Button>
-            <Button>3</Button>
-            <Button color={'gray'}>-</Button>
-            <Button color={'skyblue'} border={'left'}>.</Button>
-            <Button>0</Button>
-            <Button gridSpan={2} color={'gray'} border={'right'}>=</Button>
+            <Button onClick={handleAllClear} gridSpan={2} color={'skyblue'}>AC</Button>
+            <Button onClick={handleDelete} color={'skyblue'}>DEL</Button>
+            <Button data={'/'} onClick={chooseOperation} color={'gray'}>/</Button>
+            <Button data={'7'} onClick={appendValue}>7</Button>
+            <Button data={'8'} onClick={appendValue}>8</Button>
+            <Button data={'9'} onClick={appendValue}>9</Button>
+            <Button data={'*'} onClick={chooseOperation} color={'gray'}>*</Button>
+            <Button data={'4'} onClick={appendValue}>4</Button>
+            <Button data={'5'} onClick={appendValue}>5</Button>
+            <Button data={'6'} onClick={appendValue}>6</Button>
+            <Button data={'+'} onClick={chooseOperation} color={'gray'}>+</Button>
+            <Button data={'1'} onClick={appendValue}>1</Button>
+            <Button data={'2'} onClick={appendValue}>2</Button>
+            <Button data={'3'} onClick={appendValue}>3</Button>
+            <Button data={'-'} onClick={chooseOperation} color={'gray'}>-</Button>
+            <Button data={'.'} onClick={appendValue} color={'skyblue'} border={'left'}>.</Button>
+            <Button data={'0'} onClick={appendValue}>0</Button>
+            <Button gridSpan={2} color={'gray'} border={'right'} onClick={equals}>=</Button>
         </Container>
-     );
+    );
 }
- 
+
 export default Calculator;
